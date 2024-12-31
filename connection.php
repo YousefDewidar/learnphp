c9a9481337aaddb81fc9c04fb25e1070acc8336a
yousefdewidar <ymahmoud1213@gmail.com>
2024-12-31 04:35:28 +0200

    Update database connection details for production environment

--- connection.php
+++ connection.php
@@ -6,9 +6,9 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
-$dsn = "mysql:host=localhost;dbname=notesapp";
-$user = "root";
-$pass = "";
+$dsn = "mysql:host=sql205.infinityfree.com;dbname=if0_38003704_notesapp";
+$user = "if0_38003704";
+$pass = "1213454678790";
 $option = array(
   PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8" // FOR Arabic
 );
